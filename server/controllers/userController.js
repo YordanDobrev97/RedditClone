406f4c6bbf536711cf80e2862c63bd389bcf4868
YordanDobrev97 <yordan.19Dobrev@gmail.com>
2022-06-03 16:07:38 +0300

    Implement create comminity

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,26 @@
 const Comminity = require('../models/Comminity')
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
   comminity: {
     create: async (ctx, next) => {
       const { title } = ctx.request.body
-      const comminity = new Comminity({
-        title
-      })
+      const token = ctx.request.headers['token']
+      const userToken = jwt.decode(token)
+      
+      const comminity = new Comminity({ title })
       const res = await comminity.save()
-      ctx.body = res
+      
+      const user = await User.findOne({ _id: userToken?.userID })
+ 
+      if (!user) {
+        return ctx.body = 'user does not exist'
+      }
+      
+      user.communiticies.push(res._id)
+      await user.save()
+      ctx.body = user
       next()
     }
   }
